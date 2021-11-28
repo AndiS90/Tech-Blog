326aca9f44c08e54c5ee8532b7979f4d6627e2f3
AndiS90 <87630680+AndiS90@users.noreply.github.com>
2021-11-28 17:40:02 -0600

    got update to work

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -44,34 +44,35 @@ const delButtonHandler = async (event) => {
 
 
 const updFormHandler = async (event) => {
-  event.preventDefault();  
- 
-  const title = document.querySelector('#upd-post-title').value.trim();
-  const description = document.querySelector('#upd-post-desc').value.trim();
- 
+  event.preventDefault();
+
+
+
   if (event.target.hasAttribute('data-id')) {
     const post_id = event.target.getAttribute('data-id');
 
+    const title = document.querySelector(`#upd-post-title${post_id}`).value.trim();
+    const description = document.querySelector(`#upd-post-desc${post_id}`).value.trim();
 
-  if (title && description) {
-    const response = await fetch(`/api/posts/${post_id}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        title,
-        description
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    if (title && description) {
+      const response = await fetch(`/api/posts/${post_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title,
+          description
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to update post.');
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to update post.');
+      }
     }
   }
-}
 };
 
 
@@ -80,13 +81,16 @@ document
   .addEventListener('submit', newFormHandler);
 
 const deletebuttons = document.querySelectorAll('.del-post')
-  
-deletebuttons.forEach(function(btn){ btn.addEventListener('click', delButtonHandler);});
+
+deletebuttons.forEach(function (btn) {
+  btn.addEventListener('click', delButtonHandler);
+});
 
 
 
 
- const updateforms = document.querySelectorAll('.upd-post-form')
+const updateforms = document.querySelectorAll('.upd-post-form')
 
-updateforms.forEach(function(form){ form.addEventListener('submit', updFormHandler);});
-          
+updateforms.forEach(function (form) {
+  form.addEventListener('submit', updFormHandler);
+});
